70044af0145297dfb735e744cf65a1a7f33eea68
mdshahnawaz786 <mdshahnawaz911@gmail.com>
2023-04-06 23:15:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import A from './ChildToParent/A';
+import One from './parentToChild/One';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='Div'>
+        <h2><u>Parent to Child</u></h2>
+        <One/>
+      </div>
+      <div className='Div'>
+      <h2><u>Child to Parent</u></h2>
+        <A/>
+      </div>
     </div>
   );
 }
